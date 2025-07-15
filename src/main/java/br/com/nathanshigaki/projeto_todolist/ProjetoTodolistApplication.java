7dc23ac338de82c9956fd89c0e82d073dfb37558
Nathan <nathanshigaki@gmail.com>
2025-07-15 15:27:29 -0400

    Criação do projeto

---
+++ src/main/java/br/com/nathanshigaki/projeto_todolist/ProjetoTodolistApplication.java
@@ -0,0 +1,13 @@
+package br.com.nathanshigaki.projeto_todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTodolistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/nathanshigaki/projeto_todolist/ProjetoTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nathanshigaki.projeto_todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

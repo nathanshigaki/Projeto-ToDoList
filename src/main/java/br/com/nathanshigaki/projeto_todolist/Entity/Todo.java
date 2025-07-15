eec6887ec2c8628b1ebd9f77eb4e3b32813383a0
Nathan <nathanshigaki@gmail.com>
2025-07-15 15:53:45 -0400

    Criação do pacote entity e da classe todo

---
+++ src/main/java/br/com/nathanshigaki/projeto_todolist/Entity/Todo.java
@@ -0,0 +1,50 @@
+package br.com.nathanshigaki.projeto_todolist.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String descricao;
+    private boolean realizado;
+    private int prioridade;
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public boolean isRealizado() {
+        return realizado;
+    }
+    public void setRealizado(boolean realizado) {
+        this.realizado = realizado;
+    }
+    public int getPrioridade() {
+        return prioridade;
+    }
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
+}
